aeac06e43ac1385724d5715dc3dbc81654639e04
Eduard Sergeev <eduard.sergeev@gmail.com>
2018-09-01 19:54:43 +1000

    Add import with import list
    Handle existing import with import lists but format them when adding new import (should actually retain the existing formating TODO)

--- src/features/importProvider.ts
+++ src/features/importProvider.ts
@@ -3,16 +3,18 @@
 import * as path from 'path';
 import * as cp from 'child_process';
 import ChildProcess = cp.ChildProcess;
+import { CodeActionProvider, Disposable, DiagnosticCollection, TextDocument, Range, CodeActionContext, CancellationToken, CodeAction, WorkspaceEdit, Diagnostic, CodeActionKind, Command } from 'vscode';
 import * as vscode from 'vscode';
 
-export default class ImportProvider implements vscode.CodeActionProvider
+
+export default class ImportProvider implements CodeActionProvider
 {
 	private static commandId: string = 'haskell.addImport';
-	private command: vscode.Disposable;
-	private diagnosticCollection: vscode.DiagnosticCollection;
-	private hoogleSearch: (string, any) => any;
+	private command: Disposable;
+	private hoogleSearch: (name: string, results) => void;
 
-	public activate(subscriptions: vscode.Disposable[]) {
+	public activate(subscriptions: Disposable[])
+	{
 		this.command = vscode.commands.registerCommand(ImportProvider.commandId, this.runCodeAction, this);
 		subscriptions.push(this);
 
@@ -26,7 +28,7 @@ export default class ImportProvider implements vscode.CodeActionProvider
 		this.command.dispose();
 	}
 
-	private search(name: string): Promise<any>
+	private search(name: string): Promise<SearchResult[]>
 	{
 		var result = new Promise<any>((resolve, reject) =>
 		{
@@ -37,7 +39,7 @@ export default class ImportProvider implements vscode.CodeActionProvider
 					return {
 						package: result.getPackageName(),
 						module: result.getModuleName().replace(/-/g, '.'),
-						result: result.result 
+						result: result.result
 					};
 				}));
 			});
@@ -45,46 +47,88 @@ export default class ImportProvider implements vscode.CodeActionProvider
 		return result;
 	}
 
-	public async provideCodeActions(document: vscode.TextDocument, range: vscode.Range, context: vscode.CodeActionContext, token: vscode.CancellationToken): Promise<any>
+	public async provideCodeActions(document: TextDocument, range: Range, context: CodeActionContext, token: CancellationToken): Promise<any>
 	{
-		let diagnostic = context.diagnostics[0];
-		let patterns = [
-			/Variable not in scope:\s+(\S+)/,
-			/Not in scope: type constructor or class `(\S+)'/
-		];
-		var name = "";
-		for (let pattern of patterns)
+		let codeActions = [];
+		for (let diagnostic of context.diagnostics)
 		{
-			let match = pattern.exec(diagnostic.message);
-	  	if(match === null || match.length < 2 )
+			let patterns = [
+				/Variable not in scope:\s+(\S+)/,
+				/Not in scope: type constructor or class `(\S+)'/
+			];
+			var name = "";
+			for (let pattern of patterns)
 			{
-				continue;
+				let match = pattern.exec(diagnostic.message);
+				if (match === null)
+				{
+					continue;
+				}
+				name = match[1];
+
+				let results = await this.search(name);
+				codeActions = codeActions.concat(this.addImportForVariable(document, name, results));
+				codeActions.forEach(action =>
+				{
+					action.diagnostics = [diagnostic];
+				});
 			}
-			name = match[1];
-			
-			let results = await this.search(name);
-
-			let imports = this.getImports(document.getText());
-
-			return results.map(result =>
-			{
-				return {
-					title: "Add import " + result.module + " (for " + result.result + ")",
-					tooltip: "Add missing import",
-					command: ImportProvider.commandId,
-					arguments: [document, name, result.module]
-				};
-			});
 		}
+		return codeActions;
 	}
 
-	
-	private runCodeAction(document: vscode.TextDocument, name: string, module:string): any
+	private addImportForVariable(document: TextDocument, variableName: string, searchResults: SearchResult[]): CodeAction[]
 	{
-		function AfterMatch(offset)
+		let codeActions = [];
+		for (let result of searchResults)
+		{
+			{
+				let title = `Add "import ${result.module}" (for ${result.result})`;
+				let codeAction = new CodeAction(title, CodeActionKind.QuickFix);
+				codeAction.command = {
+					title: title,
+					command: ImportProvider.commandId,
+					arguments: [
+						document,
+						{
+							module: result.module,
+						}						
+					]
+				};
+				codeActions.push(codeAction);
+			}
+			{
+				let title = `Add "import ${result.module} ( ${variableName} )" (for ${result.result})`;
+				let codeAction = new CodeAction(title, CodeActionKind.QuickFix);
+				codeAction.command = {
+					title: title,
+					command: ImportProvider.commandId,
+					arguments: [
+						document,
+						{
+							module: result.module,
+							importElements: [variableName]
+						}						
+					]
+				};
+				codeActions.push(codeAction);
+			}
+		}
+		return codeActions;
+	}
+
+	private runCodeAction(document: TextDocument, importDeclaration: ImportDeclaration): Thenable<boolean>
+	{
+		function afterMatch(offset)
 		{
 			let position = document.positionAt(offset);
 			return document.offsetAt(position.with(position.line + 1, 0));
+		}
+
+		function importToText(importDeclaration: ImportDeclaration)
+		{
+			let importList = importDeclaration.importElements ? ` (${importDeclaration.importElements.join(", ")})` : "";
+			return `import ${importDeclaration.module}${importList}`;
 		}
 
 		let text = document.getText();
@@ -92,48 +136,85 @@ export default class ImportProvider implements vscode.CodeActionProvider
 
 		let modulePattern = /^module(.|\n)+?where/gm;
 		let moduleMatch = modulePattern.exec(text);
-		if(moduleMatch !== null)
+		if (moduleMatch !== null)
 		{
-			position = AfterMatch(moduleMatch.index + moduleMatch[0].length);
+			position = afterMatch(moduleMatch.index + moduleMatch[0].length);
 		}
 
-		for(let importInfo of this.getImports(text))
+		let edit = new WorkspaceEdit();
+
+		let oldImports = this.getImports(text);
+		let oldImport = oldImports.get(importDeclaration.module);
+		if (oldImport)
 		{
-			if(importInfo.module > module)
+			let oldElements = oldImport.importElements;
+			for (let newElem of importDeclaration.importElements)
 			{
-				position = importInfo.range.offset;
-				break;
+				let index = oldElements.findIndex(oldElem =>
+				{
+					return newElem < oldElem;
+				});
+				index = index === -1 ? oldElements.length + 1 : index;
+				oldElements.splice(index, 0, newElem);
 			}
-			position = AfterMatch(importInfo.range.offset + importInfo.range.length);
+			let oldRange = new Range(
+				document.positionAt(oldImport.offset),
+				document.positionAt(oldImport.offset + oldImport.length));
+			edit.replace(document.uri, oldRange, importToText(oldImport));
+		}
+		else
+		{
+			for (let importInfo of oldImports.values())
+			{
+				if (importInfo.module > importDeclaration.module)
+				{
+					position = importInfo.offset;
+					break;
+				}
+				position = afterMatch(importInfo.offset + importInfo.length);
+			}
+			edit.insert(document.uri, document.positionAt(position), importToText(importDeclaration) + "\n");
 		}
 
-		let edit = new vscode.WorkspaceEdit();
-		edit.insert(
-			document.uri,
-			document.positionAt(position),
-			`import ${module}\n`); 
 		return vscode.workspace.applyEdit(edit);
 	}
 
-	private *getImports(text: string): Iterable<any>
+
+	private getImports(text: string): Map<string, ImportDeclaration>
 	{
-		let importPattern = /^import\s+(?:qualified\s)?(\S+)(?:\sas\s(\S+))?\s?(?:\(((?:.|\n)*?)\))?/gm;
-		do
+		const imports = new Map();
+		const importPattern = /^import\s+(?:qualified\s)?(\S+)(?:\sas\s(\S+))?(?:\s*?\(((?:(?:\(.{1,3}?\))|.|\n)*?)\))?/gm;
+		const functionPattern = /(?:\d|\w(\((?:\.\.|(?:\d|\w|\,)*)\))?|(?:\(.+\)))+/gm;
+		for (let match; match = importPattern.exec(text);)
 		{
-			let match = importPattern.exec(text);
-			if(match === null)
-			{
-				break;
-			}
-			yield {
-				module: match[1],
+			let moduleName = match[1];
+			imports.set(moduleName, {
+				module: moduleName,
 				alias: match[2],
-				list: match[3],
-				range: {
-					offset: match.index,
-					length: match[0].length
-				}
-			};
-		} while(true);
+				importList: match[3],
+				importElements: match[3] ? match[3].match(functionPattern): null,
+				offset: match.index,
+				length: match[0].length
+			});
+		}
+		return imports;
 	}
 }
+
+interface SearchResult
+{
+	package: string;
+	module: string;
+	result: string;
+}
+
+interface ImportDeclaration
+{
+	module: string;
+	alias?: string;
+	importList?: string;
+	importElements?: string[];
+	offset?: number;
+	length?: number;
+}
+
