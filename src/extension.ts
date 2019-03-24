d5727a65ce0b0b47de72ab543b5dc4653342bacb
Eduard Sergeev <eduard.sergeev@gmail.com>
2019-03-24 13:00:10 +1100

    Add TypedHoleProvider
    Fill typed hole with one of the `valid hole fits` from GHC

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,7 @@ import OrganizeImportProvider from './features/organizeImportProvider';
 import ExtensionProvider from './features/extensionProvider';
 import OrganizeExtensionProvider from './features/organizeExtensionProvider';
 import TopLevelSignatureProvider from './features/topLevelSignatureProvider';
+import TypedHoleProvider from './features/typedHoleProvider';
 
 export function activate(context: vscode.ExtensionContext)
 {
@@ -16,6 +17,7 @@ export function activate(context: vscode.ExtensionContext)
 		new ExtensionProvider(),
 		new OrganizeExtensionProvider(),
 		new TopLevelSignatureProvider(),
+		new TypedHoleProvider(),
 	];
 	for (const provider of providers)
 	{
