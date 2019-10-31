a5b25a049abe8759dd0e510a3c943c4cc81e1fe7
Alejandro Serrano <trupill@gmail.com>
2019-10-31 13:58:36 +0100

    Support for ghcide

--- src/extension.ts
+++ src/extension.ts
@@ -11,16 +11,30 @@ import TypeWildcardProvider from './features/typeWildcardProvider';
 
 export function activate(context: vscode.ExtensionContext)
 {
-	const providers = [
-		new ImportProvider(),
-		new QualifiedImportProvider(),
-		new OrganizeImportProvider(),
-		new ExtensionProvider(),
-		new OrganizeExtensionProvider(),
-		new TopLevelSignatureProvider(),
-		new TypedHoleProvider(),
-		new TypeWildcardProvider(),
-	];
+	let isGhcIde = vscode.extensions.getExtension('DigitalAssetHoldingsLLC.ghcide') !== undefined;
+	
+	var providers;
+	if (isGhcIde) {
+		// Do not add functionality provided by ghcide itself
+		providers = [
+			new ImportProvider(),
+			new QualifiedImportProvider(),
+			new OrganizeImportProvider(),
+			new OrganizeExtensionProvider(),
+		];
+	} else {
+		providers = [
+			new ImportProvider(),
+			new QualifiedImportProvider(),
+			new OrganizeImportProvider(),
+			new ExtensionProvider(),
+			new OrganizeExtensionProvider(),
+			new TopLevelSignatureProvider(),
+			new TypedHoleProvider(),
+			new TypeWildcardProvider(),
+		];
+  }
+
 	for (const provider of providers)
 	{
 		provider.activate(context.subscriptions);
--- src/features/organizeImportProvider.ts
+++ src/features/organizeImportProvider.ts
@@ -56,7 +56,8 @@ export default class OrganizeImportProvider implements CodeActionProvider
       vscode.window.showWarningMessage(
         "Dependent extension which populates diagnostics (Errors and Warnings) is not installed.\n" +
         "Please install either [Simple GHC](https://marketplace.visualstudio.com/items?itemName=dramforever.vscode-ghc-simple) " +
-        "or [Haskero](https://marketplace.visualstudio.com/items?itemName=Vans.haskero) to enable all features.");
+        "or [Haskero](https://marketplace.visualstudio.com/items?itemName=Vans.haskero) " +
+        "or [ghcide](https://github.com/digital-asset/ghcide) to enable all features.");
     }
   }
 
@@ -71,7 +72,8 @@ export default class OrganizeImportProvider implements CodeActionProvider
   {
     const dependency =
       vscode.extensions.getExtension('dramforever.vscode-ghc-simple') ||
-      vscode.extensions.getExtension('Vans.haskero');
+      vscode.extensions.getExtension('Vans.haskero') ||
+      vscode.extensions.getExtension('DigitalAssetHoldingsLLC.ghcide');
     return dependency !== undefined;    
   }
 
