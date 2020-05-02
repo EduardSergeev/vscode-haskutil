724e8bb0d1e6090bc8ca8553fc76053dcee5884f
Eduard Sergeev <eduard.sergeev@gmail.com>
2020-05-02 21:27:09 +1000

    Add OrganizeImportProvider test

---
+++ input/OrganizeImportProvider.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module Main where
+
+import System.Environment
+import System.IO
+-- import System.IO.Unsafe
+import Control.Monad (Monad)
+import qualified Data.IntMap.Strict as M
+
+main :: IO ()
+main =
+  return ()
---
+++ input/OrganizeImportProvider_after.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module Main where
+
+import           Control.Monad (Monad)
+import qualified Data.IntMap.Strict as M
+-- import System.IO.Unsafe
+import           System.Environment
+import           System.IO
+
+main :: IO ()
+main =
+  return ()
