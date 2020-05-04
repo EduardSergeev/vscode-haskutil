989af7a133f41c28e9b84c481c44a13b832d94fe
Eduard Sergeev <eduard.sergeev@gmail.com>
2020-05-04 14:16:45 +1000

    Add `Fill type hole` test

--- input/TypeHoleProvider_after.hs
+++ input/TypeHoleProvider_after.hs
@@ -1,17 +1,7 @@
 
-f0 :: (Num a) => a -> a
--- Result: f0 :: (Num a, ()) => a -> a
--- Not idiomatic and needs -XConstraintKinds
--- Ideal: f0 :: (Num a) => a -> a or f0 :: Num a => a -> a
-f0 x = x + x
+foo :: Bool
+foo = True
 
-f3 :: (Num a, Eq a, Read a, Show a) => a -> Bool
--- Result: f3 :: (Num a, (Eq a, Read a, Show a)) => a -> Bool
--- Needs -XConstraintKinds
--- Ideal: f3 :: (Num a, Eq a, Read a, Show a) => a -> Bool
-f3 x = read (show $ x + x) == x
-
-fe :: a -> a
--- Result: fe :: () => a -> a
--- Ideal: fe :: a -> a
-fe x = x
+bar :: [a] -> [a] 
+bar xs =
+  init xs
