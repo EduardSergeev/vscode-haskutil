a9d81e880e8cab443abb96d905f27e1b7bf45179
Eduard Sergeev <eduard.sergeev@gmail.com>
2020-05-04 14:46:39 +1000

    Disable QualifiedImportProvider test
    It is extremaly flacky :-/

--- src/test/qualifiedImportProvider.test.ts
+++ src/test/qualifiedImportProvider.test.ts
@@ -3,11 +3,11 @@ import { runQuickfixTest, outputGHCiLog } from './utils';
 
 
 suite("QualifiedImportProvider", function () {
-  test("Add missing import qualified", async () => {
-    const before = path.join(__dirname, '../../input/QualifiedImportProvider.hs');
-    const after = path.join(__dirname, '../../input/QualifiedImportProvider_after.hs');
-    await runQuickfixTest(before, after, 1, 'Add: "import qualified Data.ByteString as BS"');
-  });
+  // test("Add missing import qualified", async () => {
+  //   const before = path.join(__dirname, '../../input/QualifiedImportProvider.hs');
+  //   const after = path.join(__dirname, '../../input/QualifiedImportProvider_after.hs');
+  //   await runQuickfixTest(before, after, 1, 'Add: "import qualified Data.ByteString as BS"');
+  // });
 
   teardown(async () => {
     if(this.ctx.currentTest.isFailed()) {
