54b3650d330f0ca8589676f2f8a5577b2600e130
Eduard Sergeev <Eduard.Sergeev@gmail.com>
2020-09-25 11:01:07 +1000

    Do not save input file after test run
    To avoid modified files in git status after test run
    Will do it properly (with rollback) later and only when it is needed

--- src/test/providers.test.ts
+++ src/test/providers.test.ts
@@ -1,53 +1,60 @@
 import * as vscode from 'vscode';
-import { runQuickfixTest, warmup } from './utils';
+import { runQuickfixTest } from './utils';
 
 
-suite("ExtensionProvider", function () {
-
-  suiteSetup(warmup);
-
-  test("Add missing import", async () => {
-    await runQuickfixTest('ImportProvider.hs', 3,
+suite('', () => {
+  suiteSetup(async () => {
+    await vscode.workspace.getConfiguration('ghcSimple').update('replCommand', 'ghci', true);
+    await vscode.workspace.getConfiguration('ghcSimple').update('replScope', 'file', true);
+  });
+  
+  test('Add missing import', () => {
+    return runQuickfixTest('ImportProvider.hs', 3,
       'Add: "import Data.Maybe"',
       'Add: "import Data.List (tails)"',
       'Add: "import Data.List (sort)"',
     );
   });
 
-  test("Add missing import qualified", async () => {
-    await runQuickfixTest('QualifiedImportProvider.hs', 1,
+  test('Add missing import qualified', () => {
+    return runQuickfixTest('QualifiedImportProvider.hs', 1,
       'Add: "import qualified Data.ByteString as BS"'
     );
   });
 
-  test("Organize imports", async () => {
-    await runQuickfixTest('OrganizeImportProvider.hs', 1);
+  test('Organize imports', () => {
+    return runQuickfixTest('OrganizeImportProvider.hs', 1);
   });  
-
-  test("Remove unused imports", async () => {
-    await runQuickfixTest('UnusedImportProvider.hs', 3);
+  
+  test('Remove unused imports', () => {
+    return runQuickfixTest('UnusedImportProvider.hs', 3);
   });
   
-  test("Add missing extension", async () => {
-    await runQuickfixTest('ExtensionProvider.hs', 2);
+  test('Add missing extension', () => {
+    return runQuickfixTest('ExtensionProvider.hs', 2);
   });
 
-  test("Organize extensions", async () => {
-    await runQuickfixTest('OrganizeExtensionProvider.hs', 1);
+  test('Organize extensions', () => {
+    return runQuickfixTest('OrganizeExtensionProvider.hs', 1);
   });
 
-  test("Replace wildcard with suggested type", async () => {
-    await runQuickfixTest('TypeWildcardProvider.hs', 3);
+  test('Replace wildcard with suggested type', () => {
+    return runQuickfixTest('TypeWildcardProvider.hs', 3);
   });
 
-  test("Replace type hole with suggested type", async () => {
-    await runQuickfixTest('TypeHoleProvider.hs', 2,
+  test('Replace type hole with suggested type', () => {
+    return runQuickfixTest('TypeHoleProvider.hs', 2,
       "Fill `_' with: `True'",
       "Fill `_' with: `init'"
     );
   }); 
-  
-  test("Add top-level signature", async () => {
-    await runQuickfixTest('TopLevelSignatureProvider.hs', 1);
+   
+  test('Add top-level signature', () => {
+    return runQuickfixTest('TopLevelSignatureProvider.hs', 1);
+  });  
+
+  suiteTeardown(async () => {
+    await vscode.workspace.getConfiguration('ghcSimple').update('replScope', undefined, true);
+    await vscode.workspace.getConfiguration('ghcSimple').update('replCommand', undefined, true);
   });
 });
