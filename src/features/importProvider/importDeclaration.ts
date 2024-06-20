fdf826c4ace572f047b8e7b77c715ed9e78ab25f
Eduard Sergeev <eduard.sergeev@gmail.com>
2024-06-20 19:47:31 +1200

    Improve `ImportDeclaration`
    Split imported elements list into following components:
    - `_before`: Whitespace before the first elements
    - `_importElements`: imported element names (without separators)
    - `_importSeparators`: separators between imported element names
    - `_after`: Whitespace after the last element
    
    This structure allows better handling of elements addition, removal and sorting

--- src/features/importProvider/importDeclaration.ts
+++ src/features/importProvider/importDeclaration.ts
@@ -1,7 +1,10 @@
 import { Range, TextDocument } from "vscode";
 
 export default class ImportDeclaration {
-  private _importElements?: string[] = [];
+  private _before: string = '';
+  private _importElements: string[] = [];
+  private _importSeparators: string[] = [];
+  private _after: string = '';
   qualified: string = " ";
   alias?: string;
   importList?: string;
@@ -31,53 +34,71 @@ export default class ImportDeclaration {
     }
   }
 
+  private get separator() {
+    return ', '
+  };
+
   public get importElements() {
-    return this._importElements ? this._importElements.join(',') : null;
+    const separators = this._importSeparators.concat([this._after]);
+    return [this._before].concat(this._importElements.flatMap((elem, i) => [elem, separators[i]])).join('');
   }
 
   public set importElements(elementsString: string) {
-    this._importElements = elementsString ? elementsString.split(',') : [];
-  }
-
-  public get importNames() {
-    return this._importElements.map(e => e.trim());
+    const before = /^\s*/g;
+    const elements = /[^\s,]+/g;
+    const separators = /\S([\s,]+)\S/g;
+    const after = /\s*$/g;
+    elementsString ??= '';
+    this._before = elementsString.match(before)[0];
+    this._importElements = elementsString ? [...elementsString.matchAll(elements)].map(m => m[0]) : [];
+    this._importSeparators = elementsString ? [...elementsString.matchAll(separators)].map(m => m[1]) : [];
+    this._after = elementsString.match(after)[0];
   }
 
   public addImportElement(newElem: string) {
     let before = this.importElements;
-    if (this.importNames.length === 0 || this.importNames[0] === "") {
+    if (this._importElements.length === 0) {
       this.importList = " (I)";
       before = "I";
-      this._importElements = [];
     }
 
-    let index = this.importNames.findIndex(oldElem => newElem < oldElem);
-    index = index === -1 ? this.importNames.length : index;
-    if (index === 0) {
-      if (this.importElements.length > 0) {
-        this._importElements.splice(index, 1, `${newElem}, ${this._importElements[0]}`);
-      }
-      else {
-        this._importElements.push(newElem);
+    let index = this._importElements.findIndex(oldElem => this.compareImportElements(newElem, oldElem) < 0);
+    index = index === -1 ? this._importElements.length : index;
+    if (this._importElements.length > 0) {
+      if (index === this._importElements.length) {
+        this._importSeparators.push(this.separator);
+      } else {
+        this._importSeparators.splice(index, 0, this.separator);
       }
     }
-    else {
-      this._importElements.splice(index, 0, ` ${newElem}`);
-    }
+    this._importElements.splice(index, 0, newElem);
 
     this.importList = this.importList.replace(before, this.importElements);
   }
 
   public removeElement(elem: string) {
     const before = this.importElements;
-    const index = this.importNames.findIndex(oldElem => oldElem === elem || oldElem == `${elem}(..)`);
+
+    const index = this._importElements.findIndex(oldElem => oldElem === elem || oldElem == `${elem}(..)`);
     if (index !== -1) {
-      this._importElements.splice(index, 1);
-      if(index === 0 && this._importElements.length > 0) {
-        this._importElements[0] = this._importElements[0].trimStart();
+      if (this._importElements.length > 1) {
+        if (index === this._importElements.length - 1) {
+          this._importSeparators.pop();
+        } else {
+          this._importSeparators.splice(index, 1);
+        }
       }
+      this._importElements.splice(index, 1);
+
       this.importList = this.importList.replace(before, this.importElements);
     }
+  }
+
+  private compareImportElements(left: string, right: string) {
+    const toSortable = (elem: string) => elem.replace('(', '~');
+    const ls = toSortable(left);
+    const rs = toSortable(right);
+    return ls < rs ? -1 : ls > rs ? 1 : 0;
   }
 
   public get text() {
